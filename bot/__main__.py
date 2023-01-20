c9f50b754005c5b87bb2e0b0c9d07c4670f9a34e
Lunarmagpie <bambolambo0@gmail.com>
2023-01-20 14:21:14 -0500

    change some words

--- bot/__main__.py
+++ bot/__main__.py
@@ -7,7 +7,7 @@ from bot.model import Model
 bot = hikari.GatewayBot("1234")
 
 model = Model()
-client = crescent.ClientBuilder().with_model(model).gateway(bot)
+client = crescent.ClientBuilder().with_model(model).build_gateway(bot)
 
-bot.subscribe(hikari.StartedEvent, model.on_start)
+bot.subscribe(hikari.StartingEvent, model.on_start)
 bot.start()
