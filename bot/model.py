c9eceb1e69e5a4421637a8c00de26a0530e07da3
Lunarmagpie <Bambolambo0@gmail.com>
2023-01-25 14:49:38 -0500

    Add model API to template (#1)
    * model mockup
    
    * change some words
    
    * add example
    
    * add dotenv
    
    * fix types
    
    * deps

---
+++ bot/model.py
@@ -0,0 +1,18 @@
+import aiohttp
+import hikari
+
+
+class Model:
+    def __init__(self) -> None:
+        self._session: aiohttp.ClientSession | None = None
+
+    @property
+    def session(self) -> aiohttp.ClientSession:
+        assert self._session, "aiohttp session was not started"
+        return self._session
+
+    async def on_start(self, _: hikari.StartedEvent) -> None:
+        self._session = aiohttp.ClientSession()
+
+    async def on_stop(self, _: hikari.StoppedEvent) -> None:
+        await self.session.close()
